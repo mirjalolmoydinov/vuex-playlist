a67b86eebd81e2ec61379f071970efc6f296ecc7
Mirjalol <95741797+mirjalolmoydinov@users.noreply.github.com>
2022-04-10 05:49:50 +0500

    new
    new

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="product-list-one">
+      <h2>Product List One</h2>
+      <ul>
+          <li v-for="product in products" :key="product">
+            <span class="name">{{product.name}}</span>
+            <span class="price">{{product.price}}</span>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['products'],
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="product-list-two">
+      <h2>Product List Two</h2>
+      <ul>
+          <li v-for="product in products" :key="product">
+            <span class="name">{{product.name}}</span>
+            <span class="price">{{product.price}}</span>
+
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['products'],
+
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
